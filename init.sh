5adbfa3b02f86476618261aa97080a06cc0b9ccb
linju1020 <linju1020@sina.com>
2022-06-07 13:34:38 +0800

    ljg

---
+++ init.sh
@@ -0,0 +1,13 @@
+set -e
+
+echo "PWSSWORD: $0";
+
+sudo -i
+
+echo "root:$0" | chpasswd
+
+sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
+
+sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+
+service ssh restart
